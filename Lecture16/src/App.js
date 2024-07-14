f59a027d5e15fb1a923479f7db3e0781f5ea2d8a
99Aryanshsingh <aryanshsingh925@gmail.com>
2024-07-14 14:28:35 +0530

    Add files via upload

---
+++ Lecture16/src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const Name='CipherSchool'
+  return (
+    <>
+    <div>
+      <h1>Hello from CipherSchools!</h1>
+      <h1 style={{
+        color:"red",
+        textAlign:"center"
+      }}>Hello from {Name}!</h1>
+    </div>
+    <div>
+      <h3>THis is Another Div</h3>
+    </div>
+    </>
+  );
+}
+
+export default App;
